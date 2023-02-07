7be6feb2c065a631b965aaf91669533cd7e2970f
farid <me@example.com>
2023-02-07 05:51:58 +0400

    Java

---
+++ src/lesson38_Interfaces/Interface_02.java
@@ -0,0 +1,17 @@
+package lesson38_Interfaces;
+
+public interface Interface_02 {
+
+    String MESAJ = "Java Gozeldir";
+    int REQEM = 20;
+
+    void method1();
+
+
+    // int method2();
+    String method2();
+
+    String method3();
+
+
+}
